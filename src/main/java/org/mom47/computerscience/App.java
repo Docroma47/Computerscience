168a27bdf0dc01663d4aee5c7e0521f773387173
Docroma47 <mom47@yandex.ru>
2017-07-09 22:14:26 +1200

    09.07.2017

--- src/main/java/org/mom47/computerscience/App.java
+++ src/main/java/org/mom47/computerscience/App.java
@@ -22,24 +22,51 @@ public class App
     public static void main(String[] args) {
 		App app = new App();
 		app.printInt();
+		app.printChar();
+		app.printLong();
+		app.printBoolean();
+		app.printShort();
+		app.printDouble();
+		app.printFloat();
+		app.printText();
     	// создать экземпляр (объект) класса App
 		// вызвать у объекта функцию printInt
     }
 
 	public void printInt() {
-		System.out.println(message);
 		System.out.println(intVariable);
+    }
+
+    public void printLong() {
 		System.out.println(longVariable);
+    }
+
+    public void printChar() {
 		System.out.println(charVariable);
+	}
+
+	public void printBoolean() {
 		System.out.println(booleanVariable1);
 		System.out.println(booleanVariable2);
+	}
+
+	public void printShort(){
 		System.out.println(shortVariable);
+	}
+
+	public void printDouble(){
 		System.out.println(doubleVariable1);
 		System.out.println(doubleVariable2);
+	}
+	public void printFloat(){
 		System.out.println(floatVariable);
+	}
+	public void printText(){
+		System.out.println(message);
+	}
+
 
 	}
 
 	// Добавить новые функции для каждой переменной, каждая из которой выводит на консоль свою переменную (пример printInt)
-	// Назвать каждую функцию в соответствии с именем переменной (см пример printInt)
-}
+	// Назвать каждую функцию в соответствии с именем переменной (см пример printInt)}
