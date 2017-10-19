0e5eacbde2f5b0f0b90c8a432e03f1fbdfcc2bca
Docroma47 <mom47@yandex.ru>
2017-10-20 00:35:42 +0300

    change the method using jline

--- src/main/java/org/mom47/computerscience/ConsolCalculator.java
+++ src/main/java/org/mom47/computerscience/ConsolCalculator.java
@@ -1,14 +1,26 @@
 package org.mom47.computerscience;
 
 import java.io.Console;
+import java.io.IOException;
+
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
 
 public class ConsolCalculator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Console console = System.console();
-        if (console != null){
-            String first2 = console.readLine("Please enter first number: ");
-            String second2 = console.readLine("Please enter second number: ");
-            String operation = console.readLine("Please enter operation: ");
+
+        Terminal terminal = TerminalBuilder.terminal();
+        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+
+
+        if (console != null) {
+            String first2 = reader.readLine("Please enter second number: ");
+            String second2 = reader.readLine("Please enter second number: ");
+            String operation = reader.readLine("Please enter operation: ");
             int second = Integer.valueOf(second2);
             int first = Integer.valueOf(first2);
             int result;
