91d9472fd761f5c20cb37388ca89d1dd77bede71
Docroma47 <mom47@yandex.ru>
2017-11-17 02:03:24 +0300

    Creating a file + adding text to a file

--- src/main/java/org/mom47/computerscience/FileWriterApp.java
+++ src/main/java/org/mom47/computerscience/FileWriterApp.java
@@ -1,14 +1,26 @@
 package org.mom47.computerscience;
 
-
 import java.io.Console;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileWriterApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Console console = System.console();
+
         if (args.length > 0) {
+            File file = new File(args[0]);
+            FileWriter writer = new FileWriter(file);
+            if(file.createNewFile()){
+                System.out.println(args[0] + "File create");
+            } else {
+                System.out.println(args[0] + "file not created");
+            }
             System.out.println("File name: " + args[0]);
             String text = console.readLine("Please enter some text: ");
+            writer.write(text);
+            writer.flush();
         }
     }
 }
